2317aeed2551060cd53e9f959d4f0b93245e2a47
da-moon <contact@havi.dev>
2023-06-12 23:32:30 -0400

    refactor(upstart-remount): better mount management
    - Change the `TMPDIR` to `/tmp`
    - Remove the `start on` and `stop on` clauses and replace them with a more specific `start on` clause
    - Add a directory creation command for `crew/dest` in `TMPDIR`
    - Change the mount options for all mounts in the script
    
    Signed-off-by: da-moon <contact@havi.dev>

--- upstart-remount.sh
+++ upstart-remount.sh
@@ -3,29 +3,37 @@
 UPSTART_FILE="/etc/init/remount.conf"
   cat <<EOF | sudo tee "${UPSTART_FILE}" >/dev/null
 description "Permissive mounts under user HOME"
-start on start-user-session
-stop on runlevel [!2345]
+start on (started boot-services and stopped cgroups and started lockbox-cache
+          and stopped tracefs-init)
 task
 script
 (
   for mnt in \$( $(command -v findmnt) --list --submounts --noheadings --nofsroot  --output TARGET .. );do
     (
       set -x ;
-      $(command -v mount) --internal-only --options "remount,exec,suid,symfollow" "\${mnt}"
+      $(command -v mount) --internal-only --options "remount,exec,suid,symfollow" "\${mnt}" ;
     );
   done
-    (
-      set -x ;
-      $(command -v mount) -o mode=1777,nodev -t tmpfs tmpfs "\${TMPDIR}"
-    );
+  (
+    set -x ;
+    $(command -v mount) -o mode=1777,nodev -t tmpfs tmpfs "${TMPDIR}" ;
+  );
+  (
+    set -x ;
+    mkdir -p "${TMPDIR}/crew/dest" ;
+  );
+  (
+    set -x ;
+    chown -R "$(id -un):$(id -gn)" "${TMPDIR}" ;
+  );
 ) > "\$TMPDIR/$(basename -s.conf "${UPSTART_FILE}")-\$(date +'%H-%M-%S-%N').log" 2>&1
 end script
 chdir "${HOME}"
-env TMPDIR="${TMPDIR}"
+env TMPDIR="/tmp"
 EOF
 sudo initctl stop "$(basename -s.conf "${UPSTART_FILE}")" || true
 rm -f "${TMPDIR}/$(basename -s.conf "${UPSTART_FILE}")"* || true
 sudo initctl start "$(basename -s.conf "${UPSTART_FILE}")" &
 # sleep 3 ;
 # cat "${UPSTART_FILE}"
-# tail -f "${TMPDIR}/$(basename -s.conf "${UPSTART_FILE}")"*".log"
+# tail -f "/tmp/$(basename -s.conf "${UPSTART_FILE}")"*".log"
