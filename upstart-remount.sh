0dff287a869bb554ef2cfbddfdd19e63cbfa0f04
da-moon <contact@havi.dev>
2023-06-21 20:23:27 -0400

    fix(upstart-remount): allow running executables in `TMPDIR`
    - Add a new respawn script for user sessions in `upstart-remount.sh`
    - Change `mount` calls to use absolute paths in `upstart-remount.sh`
    - Modify `upstart-remount.sh` to change ownership of `/usr/local/tmp` and `/usr/local/var/tmp` to `chronos:chronos`
    - Remove `chdir "${HOME}"` and replace it with `chdir "/home/chronos/user"` in `upstart-remount.sh`
    - Add an infinite loop in `upstart-remount.sh` to keep the script running during user sessions
    - Update `remount.conf` to start on `start-user-session` and stop on `stopping ui` in `upstart-remount.sh`
    
    Signed-off-by: da-moon <contact@havi.dev>

--- upstart-remount.sh
+++ upstart-remount.sh
@@ -1,34 +1,48 @@
 #!/usr/bin/env bash
 
+# $(command -v mount) -o mode=1777,nodev -t tmpfs tmpfs "${TMPDIR}" ;
 UPSTART_FILE="/etc/init/remount.conf"
   cat <<EOF | sudo tee "${UPSTART_FILE}" >/dev/null
 description "Permissive mounts under user HOME"
-start on (started boot-services and stopped cgroups and started lockbox-cache
-          and stopped tracefs-init)
-task
-script
+# start on (started boot-services and stopped cgroups and started lockbox-cache and stopped tracefs-init and stopped startup)
+start on start-user-session
+stop on stopping ui
+respawn
+pre-start script
 (
-  for mnt in \$( $(command -v findmnt) --list --submounts --noheadings --nofsroot  --output TARGET .. );do
+  for mnt in $( /usr/local/bin/findmnt --list --submounts --noheadings --nofsroot  --output TARGET .. );do
     (
       set -x ;
-      $(command -v mount) --internal-only --options "remount,exec,suid,symfollow" "\${mnt}" ;
+      mount --internal-only --options "remount,exec,suid,symfollow" "${mnt}" ;
     );
   done
   (
     set -x ;
-    $(command -v mount) -o mode=1777,nodev -t tmpfs tmpfs "${TMPDIR}" ;
+    mkdir -p "/usr/local/tmp" "/usr/local/var/tmp" ;
+    mount -o mode=1777,nodev -t tmpfs tmpfs "/usr/local/tmp" ;
+    mount --bind "/usr/local/var/tmp" "/usr/local/tmp"
   );
   (
     set -x ;
-    mkdir -p "${TMPDIR}/crew/dest" ;
+    mkdir -p "/usr/local/tmp/crew/dest" ;
   );
   (
     set -x ;
-    chown -R "$(id -un):$(id -gn)" "${TMPDIR}" ;
+    chown -R "chronos:chronos" "/usr/local/tmp" ;
+    chown -R "chronos:chronos" "/usr/local/var/tmp" ;
   );
-) > "\$TMPDIR/$(basename -s.conf "${UPSTART_FILE}")-\$(date +'%H-%M-%S-%N').log" 2>&1
+) > "$TMPDIR/remount-$(date +'%H-%M-%S-%N').log" 2>&1
 end script
-chdir "${HOME}"
+script
+  while :; do sleep 10; done
+end script
+pre-stop script
+(
+  umount -l "/usr/local/tmp";
+  rm -rf "/usr/local/var/tmp" ;
+)
+end script
+chdir "/home/chronos/user"
 env TMPDIR="/tmp"
 EOF
 sudo initctl stop "$(basename -s.conf "${UPSTART_FILE}")" || true
